71bea20358a5960980f7934110acbba2a1c7eae7
seshenoy <32722194+seshenoy@users.noreply.github.com>
2025-04-08 23:04:57 +0000

    Add populate_db management command to seed MongoDB with test data

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
     }
 }
 
+# Add djongo to the database engine
+DATABASES['default']['ENGINE'] = 'djongo'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
